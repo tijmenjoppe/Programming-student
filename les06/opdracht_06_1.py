db243c72fd24faefb01f7d68b1618d92aabe5faf
Tijmen Joppe Muller <tijmenjoppe@gmail.com>
2019-08-30 18:18:28 +0200

    Added template for exercise 6.1.

---
+++ les06/opdracht_06_1.py
@@ -0,0 +1,29 @@
+def som(getal1, getal2, getal3):
+    return 0
+
+
+print(som(1, 2, 3))
+print(som(-1, -2, -3))
+print(som(1, 2, -3))
+
+"""
+========================================================================================================================
+Onderstaand staan de tests voor je code -- hieronder mag je niets wijzigen!
+Je kunt je code testen door deze file te runnen of met behulp van pytest.
+"""
+import random
+
+def test_som():
+    for i in range(10):
+        lst = random.sample(range(-9, 10), 3)
+        assert som(lst[0], lst[1], lst[2]) == sum(lst), "Fout: sum({},{},{}) geeft {} in plaats van {}".format(lst[0], lst[1], lst[2], som(lst[0], lst[1], lst[2]), sum(lst))
+
+if __name__ == '__main__':
+    try:
+        print("\x1b[0;32m")
+
+        test_som()
+        print("Je functie som() doorstaat de tests! Je mag deze opdracht nu inleveren op Canvas... ")
+
+    except AssertionError as ae:
+        print("\x1b[0;31m" + str(ae))
